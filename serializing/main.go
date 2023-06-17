fba0984ed78a48b80f08a01bbcfcf7fd6636bf2a
mysd33 <mysd.33@gmail.com>
2023-06-17 18:05:49 +0900

    Composition生成処理の仮作成

--- serializing/main.go
+++ serializing/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/samply/golang-fhir-models/fhir-models/fhir"
 )
@@ -11,10 +12,15 @@ import (
 // FHIRリソースのオブジェクトをJSONシリアライズして、FHIR文書を作成するサンプル
 func main() {
 	// Bundleの作成
-	bundle := fhir.Bundle{}
-	//v, err := bundle.MarshalJSON()
+	bundle := createBundle()
+	compositionEntry := createCompositionEntry()
 
-	//改行、インデント等して出力
+	// Compositionの作成
+	bundle.Entry = append(bundle.Entry, compositionEntry)
+
+	// JSONシリアライズ
+	// v, err := bundle.MarshalJSON()
+	// 改行、インデント等して出力
 	v, err := json.MarshalIndent(bundle, "", "    ")
 
 	if err != nil {
@@ -23,4 +29,28 @@ func main() {
 
 	fmt.Println(string(v))
 
+}
+
+// Bundleリソースを作成
+func createBundle() fhir.Bundle {
+	bundle := fhir.Bundle{}
+	now := time.Now().Format("2006-01-02T15:04:05Z07:00")
+	bundle.Meta = &fhir.Meta{
+		Profile:     []string{"http://jpfhir.jp/fhir/ePrescription/StructureDefinition/JP_Bundle_ePrescriptionData"},
+		LastUpdated: &now,
+	}
+	return bundle
+}
+
+// CompositionリソースのEntryを作成
+func createCompositionEntry() fhir.BundleEntry {
+	composition := fhir.Composition{}
+	//TODO: error
+	r, _ := composition.MarshalJSON()
+
+	compositionEntity := fhir.BundleEntry{
+		Resource: r,
+	}
+
+	return compositionEntity
 }
